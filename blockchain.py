336673d36f11f7f512696de474781e29e8140e67
richard2104 <richard2104a@gmail.com>
2018-05-31 22:25:23 +0900

    Consensus added

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
+import requests
 
 ####### block generation & its principle
 
@@ -9,6 +11,7 @@ class Blockchain(object):
 	def __init__(self):
 		self.chain = []
 		self.current_transaction = []
+		self.nodes = set()
 		# genesis block
 		self.new_block(previous_hash=1, proof=100)
 
@@ -34,6 +37,46 @@ class Blockchain(object):
 			}
 		)
 		return self.last_block['index'] + 1
+
+	def register_node(self, address):
+		parsed_url = urlparse(address)
+		self.nodes.add(parsed_url.netloc) # netloc attribute! network lockation
+	
+	def valid_chain(self,chain):
+		last_block = chain[0]
+		current_index = 1
+		
+		while current_index < len(chain):
+			block = chain[current_index]
+			print(f'{last_block}')
+			print(f'{block}')
+			print("\n---------\n")
+			# check that the hash of the block is correct
+			if block['previous_hash'] != self.hash(last_block):
+				return False
+			last_block = block
+			current_index += 1
+		return True
+
+	def resolve_conflicts(self):
+		neighbours = self.nodes
+		new_chain = None
+
+		max_length = len(self.chain) # Our chain length
+		for node in neighbours:
+			response = requests.get(f'http://{node}/chain')
+			if response.status_code == 200:
+				length = response.json()['length']
+				chain = response.json()['chain']
+
+				if length > max_length and self.valid_chain(chain):
+					max_length = length
+
+			if new_chain:
+				self.chain = new_chain
+				return True
+
+			return False
 
 	# directly access from class, share! not individual instance use it
 	@staticmethod
--- server.py
+++ server.py
@@ -66,6 +66,38 @@ def full_chain():
 
 	return jsonify(response), 200
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+	values = request.get_json()
+
+	nodes = values.get('nodes')
+	if nodes is None: # Bad Request 400
+		return "Error: Please supply a valid list of nodes", 400
+
+	for node in nodes:
+		blockchain.register_node(node)
+
+	response = {
+		'message' : 'New nodes have been added',
+		'total_nodes': list(blockchain.nodes),
+	}
+	return jsonify(response), 201
+
+@app.route('nodes/resolve', methods=['GET'])
+def consensus():
+	replaced = blockchain.resolve_conflicts() # True False return
+
+	if replaced:
+		response = {
+			'message' : 'Our chain was replaced',
+			'new_chain' : blockchain.chain
+		}
+	else:
+		response = {
+			'message' : 'Our chain is authoritative',
+			'chain' : blockchain.chain
+		}
+	return jsonify(response), 200
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
