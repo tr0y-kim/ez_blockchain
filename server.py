816f929b1a8e5c7649c0981b75e44a55af259a6b
richard2104 <richard2104a@gmail.com>
2018-05-31 22:36:07 +0900

    /nodes~~

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ from blockchain import Blockchain
 # /transactions/new : to create a new transaction to a block
 # /mine : to tell our server to mine a new block.
 # /chain : to return the full Blockchain.
+# /nodes/register : to accept a list of new nodes in the form of URLs
+# /nodes/resolve : to implement our Consensus Algorithm
 
 app = Flask(__name__)
 # Universial Unique Identifier
